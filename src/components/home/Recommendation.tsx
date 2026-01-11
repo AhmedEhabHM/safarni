21fb3d9d4022eb2d02dd37e2526613c26f5db672
shaima-metwaly <shimaametwaly331@gmail.com>
2026-01-11 09:48:10 +0200

    feat: connected Recommendation & AvailableTours to backend

--- src/components/home/Recommendation.tsx
+++ src/components/home/Recommendation.tsx
@@ -1,46 +1,31 @@
-import type { RecommendationCardProps } from "@/types/home.type";
+import type {  RecommendationTourResponse, Tour } from "@/types/home.type";
 import SectionHeader from "../ui/SectionHeader"
 import TourCard from "./TourCard"
+import { useQuery } from "@tanstack/react-query";
+import { recommendationTours } from "@/services/home";
+import { TourCardSkeleton } from "@/pages/favourite/TourCardSkeleton";
 
 
 const Recommendation = () => {
-    const recommendationsData: RecommendationCardProps[] = [
-        {
-            image: 'https://images.unsplash.com/photo-1568322445389-f64ac2515020?w=500',
-            title: 'The Pyramids',
-            rating: 4.8,
-            location: 'Giza',
-            variant: "recommendation"
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1539768942893-daf53e448371?w=500',
-            title: 'The Citadel of Saladin',
-            rating: 4.8,
-            location: 'Cairo',
-            variant: "recommendation"
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1553913861-c0fddf2619ee?w=500',
-            title: 'Karnak Temple',
-            rating: 4.3,
-            location: 'Luxor',
-            variant: "recommendation"
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?w=500',
-            title: 'Library of Alexandria',
-            rating: 4.8,
-            location: 'Alexandria',
-            variant: "recommendation"
-        }
-    ];
+   
+    const { data, isLoading } = useQuery<RecommendationTourResponse>({ queryKey: ["recommendation-tours"], queryFn: recommendationTours })
+    console.log('data', data)
+    const trips:Tour[]  = data?.data ?? [];
+
     return (
         <section className="flex flex-col gap-3">
             <SectionHeader title="Recommendation" path="/" />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                {recommendationsData.map((trip, index) => {
-                    return <TourCard variant={trip.variant} key={index} image={trip.image} title={trip.title} rating={trip.rating} location={trip.location} />
-                })}
+                {isLoading ? [1, 2, 3, 4].map((_, i) => {
+                    return (
+                        <TourCardSkeleton key={i} />
+                    )
+                }) :
+                    trips.map((trip, index) => {
+                        return <TourCard isFavorite={false} variant="recommendation" key={index} image={trip.image} title={trip.title} rating={trip.rating_average} location="cairo"/>
+                    })
+                }
+
             </div>
         </section>
     )
